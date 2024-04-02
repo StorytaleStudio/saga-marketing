c66a3c305fdb74f6816e42a120e523fb0ab226bc
Josh Tregenza <josh@joshtregenza.com>
2024-04-02 10:39:03 +1000

    Update spells.js

--- js/spells.js
+++ js/spells.js
@@ -1,6 +1,6 @@
 const cardTemplateIllusion = document.querySelector('[data-user-template="illusion"]')
 
-fetch('./spells.json')
+fetch('../../../js/spells.json')
     .then(res => res.json())
     .then(data => {
         const card = cardTemplateIllusion.textContent.cloneNode(true).children[0]
