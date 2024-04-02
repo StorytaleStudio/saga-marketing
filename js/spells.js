07ab3632898e2d9fe4f8b078c51f342a4ed1e8aa
Josh Tregenza <josh@joshtregenza.com>
2024-04-02 10:48:52 +1000

    updates to bring in spells based on json

--- js/spells.js
+++ js/spells.js
@@ -1,10 +1,18 @@
 const cardTemplateIllusion = document.querySelector('[data-user-template="illusion"]')
+const spellList = document.querySelector("[data-spell-cards]")
 
 fetch('../../../js/spells.json')
     .then(res => res.json())
     .then(data => {
-        const card = cardTemplateIllusion.textContent.cloneNode(true).children[0]
-        console.log(card)
+        data.forEach(spell => {
+            const card = cardTemplateIllusion.content.cloneNode(true).children[0];
+            const name = card.querySelector("[data-name]")
+            const school = card.querySelector("[data-school]")
+            name.textContent = spell.spell_name;
+            school.textContent = spell.school;
+            spellList.append(card)
+            console.log(card)
+        })
     });
 
 
