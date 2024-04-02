a0fac8dcaac3c4990a0a49cf1b73646a9b4ec972
Josh Tregenza <josh@joshtregenza.com>
2024-04-02 12:01:14 +1000

    bringing in search functionality

--- js/spells.js
+++ js/spells.js
@@ -8,6 +8,13 @@ const cardTemplateConjuration = document.querySelector('[data-user-template="con
 const cardTemplateNecromancy = document.querySelector('[data-user-template="necromancy"]')
 
 const spellList = document.querySelector("[data-spell-cards]")
+const searchInput = document.querySelector("[data-search]");
+
+searchInput.addEventListener("input", e=> {
+    const value = e.target.value;
+    console.log(value)
+})
+
 
 fetch('../../../js/spells.json')
     .then(res => res.json())
