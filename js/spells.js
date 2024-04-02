801a02d0aebcac33ed0901ce703e578ff8e4cc07
Josh Tregenza <josh@joshtregenza.com>
2024-04-02 11:29:34 +1000

    updated spells

--- js/spells.js
+++ js/spells.js
@@ -1,11 +1,37 @@
 const cardTemplateIllusion = document.querySelector('[data-user-template="illusion"]')
+const cardTemplateEvocation = document.querySelector('[data-user-template="evocation"]')
+const cardTemplateAbjuration = document.querySelector('[data-user-template="abjuration"]')
+const cardTemplateDivination = document.querySelector('[data-user-template="divination"]')
+const cardTemplateEnchantment = document.querySelector('[data-user-template="enchantment"]')
+const cardTemplateTransmutation = document.querySelector('[data-user-template="transmutation"]')
+const cardTemplateConjuration = document.querySelector('[data-user-template="conjuration"]')
+const cardTemplateNecromancy = document.querySelector('[data-user-template="necromancy"]')
+
 const spellList = document.querySelector("[data-spell-cards]")
 
 fetch('../../../js/spells.json')
     .then(res => res.json())
     .then(data => {
         data.forEach(spell => {
-            const card = cardTemplateIllusion.content.cloneNode(true).children[0];
+            const card = '';
+            if (spell.school == "illusion") {
+                card = cardTemplateIllusion.content.cloneNode(true).children[0];
+            } else if (spell.school == 'evocation') {
+                card = cardTemplateEvocation.content.cloneNode(true).children[0];
+            } else if (spell.school == 'abjuration') {
+                card = cardTemplateAbjuration.content.cloneNode(true).children[0];
+            } else if (spell.school == 'divination') {
+                card = cardTemplateDivination.content.cloneNode(true).children[0];
+            } else if (spell.school == 'enchantment') {
+                card = cardTemplateEnchantment.content.cloneNode(true).children[0];
+            } else if (spell.school == 'transmutation') {
+                card = cardTemplateTransmutation.content.cloneNode(true).children[0];
+            } else if (spell.school == 'conjuration') {
+                card = cardTemplateConjuration.content.cloneNode(true).children[0];
+            } else {
+                card = cardTemplateNecromancy.content.cloneNode(true).children[0];
+            }
+            
             const name = card.querySelector("[data-name]")
             const school = card.querySelector("[data-school]")
             const level = card.querySelector("[data-level]")
