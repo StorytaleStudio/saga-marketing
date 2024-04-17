fae68b86a43ba5e22b381157b6ae06e3921003cb
Josh Tregenza <josh@joshtregenza.com>
2024-04-17 12:42:57 +1000

    Update spells.js

--- js/spells.js
+++ js/spells.js
@@ -32,9 +32,112 @@ var checked = [];
 //         console.log(checkVal)
 //     })
 // })
-function checked() {
-    console.log("Hey")
-}
+function change() {
+    var levelCbs = document.querySelectorAll(".level-wrapper input[type='checkbox']");
+    var classCbs = document.querySelectorAll(".class-wrapper input[type='checkbox']");
+    var typeCbs = document.querySelectorAll(".type-wrapper input[type='checkbox']");
+    var filters = {
+      level: getClassOfCheckedCheckboxes(levelCbs),
+      class: getClassOfCheckedCheckboxes(classCbs),
+      type: getClassOfCheckedCheckboxes(typeCbs)
+
+    };
+  
+    filterResults(filters);
+  }
+  
+  function getClassOfCheckedCheckboxes(checkboxes) {
+    var classes = [];
+  
+    if (checkboxes && checkboxes.length > 0) {
+      for (var i = 0; i < checkboxes.length; i++) {
+        var cb = checkboxes[i];
+  
+        if (cb.checked) {
+          classes.push(cb.getAttribute("data-name"));
+        }
+      }
+    }
+  
+    return classes;
+  }
+  
+  function filterResults(filters) {
+    var rElems = spellList;
+    var hiddenElems = [];
+  
+    if (!rElems || rElems.length <= 0) {
+      return;
+    }
+  
+    for (var i = 0; i < rElems.length; i++) {
+      var el = rElems[i];
+  
+      if (filters.class.length > 0) {
+        var isHidden = true;
+  
+        for (var j = 0; j < filters.class.length; j++) {
+          var filter = filters.class[j];
+  
+          if (el.classList.contains(filter)) {
+            isHidden = false;
+            break;
+          }
+        }
+  
+        if (isHidden) {
+          hiddenElems.push(el);
+        }
+      }
+  
+      if (filters.level.length > 0) {
+        var isHidden = true;
+  
+        for (var j = 0; j < filters.level.length; j++) {
+          var filter = filters.level[j];
+  
+          if (el.classList.contains(filter)) {
+            isHidden = false;
+            break;
+          }
+        }
+  
+        if (isHidden) {
+          hiddenElems.push(el);
+        }
+      }
+
+
+      if (filters.type.length > 0) {
+        var isHidden = true;
+  
+        for (var j = 0; j < filters.type.length; j++) {
+          var filter = filters.level[j];
+  
+          if (el.classList.contains(filter)) {
+            isHidden = false;
+            break;
+          }
+        }
+  
+        if (isHidden) {
+          hiddenElems.push(el);
+        }
+      }
+    }
+  
+    for (var i = 0; i < rElems.length; i++) {
+      rElems[i].style.display = "block";
+    }
+  
+    if (hiddenElems.length <= 0) {
+      return;
+    }
+  
+    for (var i = 0; i < hiddenElems.length; i++) {
+      hiddenElems[i].style.display = "none";
+    }
+  }
 
 
 const kebabCase = string => string
